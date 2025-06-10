f6a6e56cb413fdd9fd1e24e32ebe2229bf2bb9da
“fashaap” <bmpasha23@gmail.com>
2025-06-10 13:09:01 -0700

    update & add

--- src/components/layouts/MainLayouts.jsx
+++ src/components/layouts/MainLayouts.jsx
@@ -32,109 +32,113 @@ const MainLayouts = () => {
   // console.log("width", width);
 
   const NavigationMenu = {
-    home: { path: "/", label: "Home" },
-    about: { path: "/about", label: "About Us" },
-    donation: { path: "/donation", label: "Donation" },
-    ticket: { path: "/ticket", label: "Ticket" },
-    newsArticle: { path: "/news-article", label: "News & Article" },
-    login: { path: "/login", label: "Login" },
+    home: { id: 1, path: "/", label: "Home" },
+    about: { id: 2, path: "/about", label: "About Us" },
+    donation: { id: 3, path: "/donation", label: "Donation" },
+    ticket: { id: 4, path: "/ticket", label: "Ticket" },
+    newsArticle: { id: 5, path: "/news-article", label: "News & Article" },
   };
 
   const NavigationBar = () => {
+    const handleMenuClick = () => setIsMenuOpen(true);
+
     return (
       <div
-        className={`flex fixed top-15 z-10 w-full ${
-          !isMenuOpen && width <= 830 ? "h-80 shadow-2xl" : ""
-        } bg-white shadow-md `}
+      className={`flex fixed top-15 z-10 w-full ${
+        !isMenuOpen && width <= 830 ? "h-80 shadow-2xl" : ""
+      } bg-white shadow-md `}
       >
-        <div
-          className={` fixed top-0 z-10 bg-white shadow-md flex items-center justify-between px-7  w-full  ${
-            scrollY >= 35
-              ? "w-full h-20"
-              : "xl:container xl:w-[60%] h-16 xl:left-1/2 xl:-translate-x-1/2 xl:rounded-full xl:mt-5 "
+      <div
+        className={` fixed top-0 z-10 bg-white shadow-md flex items-center justify-between px-7  w-full  ${
+        scrollY >= 35
+          ? "w-full h-16 md:h-20"
+          : "xl:container xl:w-[60%] h-16 xl:left-1/2 xl:-translate-x-1/2 xl:rounded-full xl:mt-5 "
+        }`}
+      >
+        <Link to="/" onClick={handleMenuClick}>
+        <img
+          src={logoTht}
+          alt="Logo THT"
+          className={`${
+          scrollY <= 35 ? "lg:h-14" : "md:h-16"
+          } transition-all duration-300 h-12`}
+        />
+        </Link>
+        {width >= 830 ? (
+        <>
+          <nav
+          className={`flex space-x-4 items-center  transition-all duration-300 ${
+            scrollY >= 35 ? "text-lg" : "text-md"
           }`}
-        >
-          <Link to="/">
-            <img
-              src={logoTht}
-              alt="Logo THT"
-              className={`${
-                scrollY <= 35 ? "h-14" : "h-16"
-              } transition-all duration-300`}
-            />
-          </Link>
-          {width >= 830 ? (
-            <>
-              <nav
-                className={`flex space-x-4 items-center  transition-all duration-300 ${
-                  scrollY >= 35 ? "text-lg" : "text-md"
-                }`}
-              >
-                <Link to="/" className="text-orange-600 hover:underline">
-                  Home
-                </Link>
-                <Link to="/about" className="text-orange-600 hover:underline">
-                  About Us
-                </Link>
-                <Link
-                  to="/donation"
-                  className="text-orange-600 hover:underline"
-                >
-                  Donation
-                </Link>
-                <Link to="/Ticket" className="text-orange-600 hover:underline">
-                  Ticket
-                </Link>
-                <Link to="/contact" className="text-orange-600 hover:underline">
-                  News & Article
-                </Link>
-                <Link to="/contact" className="text-orange-600 hover:underline">
-                  News & Article
-                </Link>
-              </nav>
-              <div className="flex items-center space-x-2 ">
-                <Link
-                  to="/login"
-                  className={`text-orange-600 font-semibold underline  transition-all duration-300 ${
-                    scrollY >= 35 ? "text-lg" : "text-md"
-                  }`}
-                >
-                  Login
-                </Link>
-              </div>
-            </>
-          ) : (
-            <button
-              type="button"
-              onClick={() => {
-                setIsMenuOpen(!isMenuOpen);
-              }}
-              className="flex items-center justify-center w-10 h-10 bg-white "
+          >
+          {Object.values(NavigationMenu).map((item) => (
+            <Link
+            key={item.id}
+            to={item.path}
+            className={`text-orange-600  hover:underline  transition-all duration-300 ${
+              scrollY >= 35 ? "text-lg" : "text-md"
+            }`}
+            onClick={handleMenuClick}
             >
-              {isMenuOpen ? (
-                <img className="h-8" src={burgerMenu} alt="menu_icon" />
-              ) : (
-                <img
-                  className="h-8"
-                  src={bugerMenuClose}
-                  alt="menu_close_icon"
-                />
-              )}
-            </button>
-          )}
-        </div>
-        {!isMenuOpen && width <= 830 ? (
-          <div className="flex flex-col gap-3 items-center justify-center w-full h-full">
-            <Link className="text-orange-600 text-lg">Home</Link>
-            <Link className="text-orange-600 text-lg">About Us</Link>
-            <Link className="text-orange-600 text-lg">Donation</Link>
-            <Link className="text-orange-600 text-lg">Ticket</Link>
-            <Link className="text-orange-600 text-lg">News & Article</Link>
-            <Link className="text-orange-600 text-lg font-semibold underline ">
-              Login
+            {item.label}
             </Link>
+          ))}
+          </nav>
+          <div className="flex items-center space-x-2 ">
+          <Link
+            to="/login"
+            className={`text-orange-600 font-semibold underline  transition-all duration-300 ${
+            scrollY >= 35 ? "text-lg" : "text-md"
+            }`}
+            onClick={handleMenuClick}
+          >
+            Login
+          </Link>
           </div>
-        ) : null}
+        </>
+        ) : (
+        <button
+          type="button"
+          onClick={() => {
+          setIsMenuOpen(!isMenuOpen);
+          }}
+          className="flex items-center justify-center w-10 h-10 bg-white "
+        >
+          {isMenuOpen ? (
+          <img className="h-8" src={burgerMenu} alt="menu_icon" />
+          ) : (
+          <img
+            className="h-8"
+            src={bugerMenuClose}
+            alt="menu_close_icon"
+          />
+          )}
+        </button>
+        )}
+      </div>
+      {!isMenuOpen && width <= 830 ? (
+        <div className="flex flex-col gap-3 items-center justify-center w-full h-full">
+        <nav className="flex flex-col items-center justify-center space-y-3">
+          {Object.values(NavigationMenu).map((item) => (
+          <Link
+            key={item.id}
+            to={item.path}
+            className="text-orange-600 hover:underline transition-all duration-300 text-lg"
+            onClick={handleMenuClick}
+          >
+            {item.label}
+          </Link>
+          ))}
+        </nav>
+        <Link
+          to="/login"
+          className="text-orange-600 font-semibold underline transition-all duration-300 text-lg"
+          onClick={handleMenuClick}
+        >
+          Login
+        </Link>
+        </div>
+      ) : null}
       </div>
     );
   };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,56 +1,85 @@
-import backgroundImage from "../../assets/JPEG-2000.png";
+import { useEffect, useState } from "react";
+import backgroundImage from "../../assets/JPEG-2000.webp";
 import logoTht from "../../assets/LOGO_THT_2_hitam.svg";
 import PageLayouts from "../../components/layouts/PageLayouts";
 
 const HomePage = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  console.log("width", width);
+
+  const content1 = [
+    {
+      id: 1,
+      title: "Walking Tour Exploring the History of Afdeeling Tjicalengka",
+    },
+    {
+      id: 2,
+      title: "Local Vacation",
+    },
+    {
+      id: 3,
+      title: "Tjicalengka Historical Camp",
+    },
+    {
+      id: 4,
+      title: "Ikuti Keseruan Lainnya!",
+    },
+  ];
+
   return (
     <div className="flex flex-col ">
       <div
-        className="bg-cover lg:bg-position-[center_bottom_-200px] xl:bg-position-[center_bottom_-350px] md:h-120 flex items-center justify-center bg-no-repeat"
+        className="bg-cover md:bg-position-[center_bottom_-80px] lg:bg-position-[center_bottom_-140px] xl:bg-position-[center_bottom_-320px] h-100 md:h-120 flex items-center justify-center bg-no-repeat"
         style={{ backgroundImage: `url(${backgroundImage})` }}
       >
-        <div className="bg-gradient-to-b from-transparent to-black/50 w-full h-120" />
+        <div className="bg-gradient-to-b from-transparent to-black/50 w-full h-100 md:h-120" />
       </div>
       <PageLayouts>
-        <div className="my-34 ">
-          <h1 className="text-5xl text-center font-bold text-white text-shadow-lg/30 ">
+        <div className="my-25 md:my-30 px-2">
+          <h1
+            className="text-3xl md:text-4xl lg:text-5xl text-center font-bold text-white text-shadow-lg/30"
+          >
             Selamat datang di website resmi kami
           </h1>
-          <p className="text-center text-2xl text-white text-shadow-lg/30">
+          <p className="text-center text-sm md:text-xl xl:text-2xl text-white text-shadow-lg/30">
             Menelusuri Jejak Waktu Lewat Cerita & Warisan Cicalengka
           </p>
-          <p className="text-center text-xl text-white  text-shadow-lg/30">
+          <p className="text-center text-sm md:text-md lg:text-lg xl:text-xl text-white  text-shadow-lg/30">
             "Kami Bangga Sedjarah Kota Ketjil Kami!"
           </p>
         </div>
+
         <div className="flex justify-center w-full h-auto shadow-sm bg-white py-4">
-          <div className="container lg:mx-40 xl:mx-60 px-4 gap-3 w-full justify-center flex items-center">
-            <img className="h-20 mx-8" src={logoTht} alt="logo_tht_2_orange" />
+          <div className="container w-full grid md:flex lg:mx-40 xl:mx-60 px-4 gap-3  justify-center  items-center">
+            <img
+              className="h-20 xl:mx-8 mx-auto"
+              src={logoTht}
+              alt="logo_tht_2_orange"
+            />
 
-            <div className="flex items-center  justify-center w-60 h-full px-3 py-3   bg-white rounded-xl border transition  ">
-              <h1 className="text-lg text-center font-bold   ">
-                Walking Tour Exploring the History of Afdeeling Tjicalengka
-              </h1>
-            </div>
-
-            <div className="flex items-center  justify-center w-60 h-full px-3 py-3   bg-white rounded-xl border transition">
-              <h1 className="text-md text-center font-bold ">
-                Local Vacation
-              </h1>
-            </div>
-
-            <div className="flex items-center  justify-center w-60 h-full px-3 py-3   bg-white rounded-xl border transition">
-              <h1 className="text-md text-center font-bold ">
-                Tjicalengka Historical Camp
-              </h1>
-            </div>
-
-            <div className="flex items-center  justify-center w-60 h-full px-3 py-3  bg-white rounded-xl border transition">
-              <h1 className="text-md text-center font-bold ">
-                Ikuti Keseruan Lainnya
-              </h1>
-            </div>
+            {Object.values(content1).map((item) => (
+              <div
+                key={item.id}
+                className="flex items-center justify-center w-full lg:w-60 h-25 md:h-full px-3 py-3 bg-white rounded-xl border transition hover:scale-102 hover:shadow-lg "
+              >
+                <h1 className="text-sm md:text-md lg:text-lg text-center font-bold  ">
+                  {item.title}
+                </h1>
+              </div>
+            ))}
           </div>
+        </div>
+
+        <div className="w- px-4 flex  mt-5 ">
+          <h1 className="">Menjaga Jejak, Menghidupkan Sejarah</h1>
         </div>
       </PageLayouts>
     </div>
